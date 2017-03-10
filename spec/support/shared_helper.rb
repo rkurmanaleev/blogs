4354f4bd4c5be76647768942ae160519972bbc48
Ruslan Kurmanaleev <ruslan.kurmanaleev@yandex.ru>
2017-03-10 11:10:48 +0300

    Add shared helper (sign_in user)

---
+++ spec/support/shared_helper.rb
@@ -0,0 +1,11 @@
+module SharedHelper
+
+  def sign_in(user)
+    visit new_user_registration_path
+
+    fill_in 'email', with: user.email
+    fill_in 'password', with: user.password
+    click_on 'Sign in'
+  end
+
+end
