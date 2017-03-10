358681f365b31f820e725247c17c5f5196314f17
Ruslan Kurmanaleev <ruslan.kurmanaleev@yandex.ru>
2017-03-10 18:34:12 +0300

    Refactor the Post create spec

--- spec/features/user/posts/create_spec.rb
+++ spec/features/user/posts/create_spec.rb
@@ -3,21 +3,22 @@ require "rails_helper"
 feature "User creates a Post" do
   include_context "current user signed in"
 
-  scenario "with valid attributes" do
+  def fill_new_post_attributes(title)
     visit new_post_path
-    fill_in "Title", with: "Test title"
+    fill_in "Title", with: title
     fill_in "Content", with: "Some kind of content for test purposes"
     click_on "Create Post"
+  end
+
+  scenario "with valid attributes" do
+    fill_new_post_attributes("Some cool title")
 
     expect(current_path).to eq posts_path
     expect(page).to have_content "Post has been successfully created."
   end
 
   scenario "with invalid attributes" do
-    visit new_post_path
-    fill_in "Title", with: nil
-    fill_in "Content", with: "Some kind of a title"
-    click_on "Create Post"
+    fill_new_post_attributes(nil)
 
     expect(current_path).to eq posts_path
     expect(page).to have_content "Title can't be blank"
