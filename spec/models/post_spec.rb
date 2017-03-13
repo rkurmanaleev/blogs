f5c507ba465a05c025763695537e4056acbb4670
Ruslan Kurmanaleev <ruslan.kurmanaleev@yandex.ru>
2017-03-13 11:07:45 +0300

    Update model specs to reflect Comment model

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,7 +1,8 @@
 require "rails_helper"
 
 describe Post do
-  it { is_expected.to belong_to :user }
   it { is_expected.to validate_presence_of :title }
   it { is_expected.to validate_presence_of :content }
+  it { is_expected.to belong_to :user }
+  it { is_expected.to have_many :comments}
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,4 +3,5 @@ require "rails_helper"
 describe User do
   it { is_expected.to validate_presence_of :full_name }
   it { is_expected.to have_many :posts }
+  it { is_expected.to have_many :comments }
 end
