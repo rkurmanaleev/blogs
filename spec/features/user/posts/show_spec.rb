37fd5bff246ec8e26965e05ebe63a80271865480
Ruslan Kurmanaleev <ruslan.kurmanaleev@yandex.ru>
2017-03-13 13:27:19 +0300

    Refactor the Post show spec

--- spec/features/user/posts/show_spec.rb
+++ spec/features/user/posts/show_spec.rb
@@ -2,35 +2,42 @@ require "rails_helper"
 
 feature "As user I want to view my previous post with comments" do
   include_context "current user signed in"
-  let!(:post) { create(:post, user: current_user, title: "The post to show") }
+  include_context "post with comment"
+
+  RSpec::Matchers.define :have_posts_content_and_title_on_post_page do
+    match do
+      eq post_path(post)
+      have_content post.title
+      have_content post.content
+    end
+  end
+
+  def visit_posts_path_and_click_on_title
+    visit posts_path
+    click_on post.title
+  end
 
   context "Route behaviour" do
     scenario "via link from home_page" do
-      visit posts_path
-      expect(page).to have_content post.title
-      click_on "The post to show"
+      visit_posts_path_and_click_on_title
 
-      expect(current_path).to eq post_path(post)
-      expect(page).to have_content post.content
+      expect(page).to have_posts_content_and_title_on_post_page
     end
 
     scenario "via direct link" do
       visit post_path(post)
 
-      expect(current_path).to eq post_path(post)
-      expect(page).to have_content post.content
+      expect(page).to have_posts_content_and_title_on_post_page
     end
   end
 
   context "I can see comments" do
-    let!(:comment) { create(:comment, user: current_user, post: post) }
+    let(:posts_comments) { find(".posts-comments") }
 
     scenario "to each post" do
       visit post_path(post)
 
-      within ".posts-comments" do
-        expect(page).to have_content comment.content
-      end
+      expect(posts_comments).to have_content comment.content
     end
   end
 end
