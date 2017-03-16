0b079cd9dc2bf7a0a98bf270410403b72e2b1ba9
Ruslan Kurmanaleev <ruslan.kurmanaleev@yandex.ru>
2017-03-16 12:38:08 +0300

    Refactor the Comment Decorator

--- app/decorators/comment_decorator.rb
+++ app/decorators/comment_decorator.rb
@@ -1,5 +1,10 @@
 class CommentDecorator < ApplicationDecorator
-  delegate :id, :content, :post, :user, :created_on, :updated_on
+  delegate :content, :created_on, :updated_on
+  delegate :full_name, to: :user, prefix: true
+
+  def user_full_name
+    @user_full_name = object.user.full_name
+  end
 
   def formatted_date
     object.created_at.strftime("%Y-%m-%d %H:%M")
