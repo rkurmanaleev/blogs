9ac8aaf0b4872695fd56674deb930a0695437652
Ruslan Kurmanaleev <ruslan.kurmanaleev@yandex.ru>
2017-03-20 15:04:39 +0300

    Finished the story show link to all posts

--- spec/features/user/show_spec.rb
+++ spec/features/user/show_spec.rb
@@ -2,12 +2,26 @@ require "rails_helper"
 
 feature "See any user's posts" do
   include_context "current user signed in"
-  let!(:post) { create(:post, user: current_user) }
+  let!(:posts) { create_list(:post, 12, user: current_user) }
+  let(:first_post) { posts.first }
+  let(:last_post) { posts.last }
 
-  scenario "As authenticated User" do
-    visit user_path(current_user)
+  context "Default behavior - recent 10 posts" do
+    scenario "As authenticated User" do
+      visit user_path(current_user)
 
-    expect(page).to have_content post.title
-    expect(page).to have_content post.content
+      expect(page).to have_content last_post.title
+      expect(page).not_to have_content first_post.title
+    end
+  end
+
+  context "All posts" do
+    scenario "As authenticated User" do
+      visit user_path(current_user)
+      click_on "See all posts"
+
+      expect(page).to have_content first_post.title
+      expect(page).to have_content last_post.title
+    end
   end
 end
