4a89477fabecef3340a09bfe4244867a53e95fe0
Ruslan Kurmanaleev <ruslan.kurmanaleev@yandex.ru>
2017-03-20 09:46:36 +0300

    Finished the Create Comment via JS story

--- spec/features/user/posts/comments/create_spec.rb
+++ spec/features/user/posts/comments/create_spec.rb
@@ -1,14 +1,28 @@
 require "rails_helper"
 
-feature "As user I want to create new comment" do
+feature "As user I want to create new comment to the post at the bottom with" do
   include_context "current user signed in"
   let!(:post) { create(:post, user: current_user) }
 
-  scenario "to a post using the form at the bottom of post page", js: true do
+  def fill_form(title = nil)
     visit post_path(post)
-    fill_in "comment_content", with: "Awesome blog and the post!"
+    fill_in "content-field", with: title
     click_on "Save Comment"
+  end
 
+  scenario "valid params", js: true do
+    fill_form("Awesome blog and the post!")
+
+    expect(page).not_to have_content "No comments yet..."
     expect(page).to have_content "Awesome blog and the post!"
+    expect(page).to have_content "Successfully created a comment"
+  end
+
+  scenario "invalid params", js: true do
+    fill_form
+
+    expect(page).to have_content "No comments yet..."
+    expect(page).not_to have_content "Awesome blog and the post!"
+    expect(page).to have_content "Text field couldn't be blank!"
   end
 end
