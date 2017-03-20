41e3c3cb9cb3004211f98aedb62213f41ecab3db
Ruslan Kurmanaleev <ruslan.kurmanaleev@yandex.ru>
2017-03-20 09:46:16 +0300

    Add comments to seeds

--- db/seeds/development/all.seeds.rb
+++ db/seeds/development/all.seeds.rb
@@ -1,3 +1,6 @@
 FactoryGirl.create(:user, email: "test@test.test", password: 'Testtest')
+FactoryGirl.create(:user, email: "testtest@test.test", password: 'Testtest')
 FactoryGirl.create_list(:post, 13, user: User.first)
+FactoryGirl.create_list(:post, 3, user: User.last)
 FactoryGirl.create(:comment, user: User.first, post: Post.last)
+FactoryGirl.create_list(:comment, 5, user: User.last, post: Post.last)
