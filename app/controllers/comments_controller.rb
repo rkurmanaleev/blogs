6824c633fdc8a479a9acaa54af8669e5c68a22c3
Ruslan Kurmanaleev <ruslan.kurmanaleev@yandex.ru>
2017-03-21 11:17:29 +0300

    Comments#destroy method

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,36 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_user!
+
+  expose(:comment)
+  expose(:post)
+  expose(:comments) { comments_fetch }
+  expose(:user)
+
+  respond_to :json
+
+  def create
+    if comment.save
+      render json: comment, include: { user: { only: :full_name } }, status: 200
+    else
+      render json: comment.errors, status: 422
+    end
+  end
+
+  def destroy
+    if comment.destroy
+      render json: comment, status: :ok
+    else
+      render json: comment.errors, status: 422
+    end
+  end
+
+  private
+
+  def comments_fetch
+    post.comments.ordered_by_desc
+  end
+
+  def comment_params
+    params.require(:comment).permit(:id, :content, :post_id, :user_id)
+  end
+end
