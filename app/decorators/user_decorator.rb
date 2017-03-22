39bdee86a5508edf13990c5d7181c0fda5145d8a
Ruslan Kurmanaleev <ruslan.kurmanaleev@yandex.ru>
2017-03-22 12:05:57 +0300

    Refactor the User Decorator

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -2,7 +2,7 @@ class UserDecorator < ApplicationDecorator
   delegate :id, :full_name, :email
 
   def full_name_with_email
-    @full_name_with_email = "#{object.full_name} (#{object.email})"
+    @full_name_with_email ||= "#{object.full_name} (#{object.email})"
   end
 
   def posts_stat
