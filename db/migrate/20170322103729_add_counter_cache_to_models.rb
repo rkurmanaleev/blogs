ccaa37962a8b813de849e174216300d1f8d53cb5
Ruslan Kurmanaleev <ruslan.kurmanaleev@yandex.ru>
2017-03-22 13:53:16 +0300

    Add int fields for counter_cache

---
+++ db/migrate/20170322103729_add_counter_cache_to_models.rb
@@ -0,0 +1,7 @@
+class AddCounterCacheToModels < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :posts_count, :integer
+    add_column :users, :comments_count, :integer
+    add_column :posts, :comments_count, :integer
+  end
+end
