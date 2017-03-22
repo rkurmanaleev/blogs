003c7bbeac690dbc7265ab3424cf74389d8a48b5
Ruslan Kurmanaleev <ruslan.kurmanaleev@yandex.ru>
2017-03-22 11:58:44 +0300

    Refactor the User Show spec

--- spec/features/user/show_spec.rb
+++ spec/features/user/show_spec.rb
@@ -1,27 +1,30 @@
 require "rails_helper"
 
-feature "See any user's posts" do
+feature "As authenticated User, I can see any user's post" do
   include_context "current user signed in"
   let!(:posts) { create_list(:post, 12, user: current_user) }
-  let(:first_post) { posts.first }
   let(:last_post) { posts.last }
+  let(:first_post) { posts.first }
+  before { visit user_path(current_user) }
 
-  context "Default behavior - recent 10 posts" do
-    scenario "As authenticated User" do
-      visit user_path(current_user)
+  scenario "Recent 10 posts" do
+    click_on "See Recent Posts"
 
-      expect(page).to have_content last_post.title
-      expect(page).not_to have_content first_post.title
-    end
+    expect(current_path).to eq user_recent_posts_path(current_user)
+    expect(page).to have_content last_post.title
+    expect(page).not_to have_content first_post.title
   end
 
-  context "All posts" do
-    scenario "As authenticated User" do
-      visit user_path(current_user)
-      click_on "See all posts"
+  scenario "All posts" do
+    click_on "See All Posts"
 
-      expect(page).to have_content first_post.title
-      expect(page).to have_content last_post.title
-    end
+    expect(current_path).to eq user_all_posts_path(current_user)
+    expect(page).to have_content first_post.title
+    expect(page).to have_content last_post.title
+  end
+
+  scenario "Statistics" do
+    expect(page).to have_content "Total Posts: #{current_user.posts.count}"
+    expect(page).to have_content "Total Comments: #{current_user.comments.count}"
   end
 end
