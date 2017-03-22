83fc3d8f0b415a68cb376c6803dee64ad4989124
Ruslan Kurmanaleev <ruslan.kurmanaleev@yandex.ru>
2017-03-22 11:56:57 +0300

    Refactor the Users Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,4 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-
   expose_decorated(:user)
-  expose_decorated(:posts) { posts_fetcher }
-
-  private
-
-  def posts_fetcher
-    params[:all] ? user.posts.ordered_by_desc : user.posts.ordered_by_desc.latest
-  end
 end
