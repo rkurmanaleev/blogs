ed9e6ec5c2d20ef1eadf6f87357b587bb7a9ffa0
Ruslan Kurmanaleev <ruslan.kurmanaleev@yandex.ru>
2017-04-20 11:45:05 +0300

    Add specs and update views for STATIC avatar uplod

---
+++ spec/features/user/account/update_avatar_spec.rb
@@ -0,0 +1,30 @@
+require "rails_helper"
+
+feature "As the user I would like to change my Avatar" do
+  include_context "current user signed in"
+
+  def change_avatar(folder, file)
+    visit edit_user_registration_path(current_user)
+    attach_file("user_avatar_image", Rails.root.join("app", "assets", folder, file))
+    click_on "Update"
+  end
+
+  scenario "with the picture successfully" do
+    change_avatar("images", "no-avatar.png")
+
+    expect(page).to have_content "Your account has been updated successfully."
+
+    visit edit_user_registration_path(current_user)
+    within ".user-avatar" do
+      expect(find("img")["src"]).to have_content "avatar_image.jpg"
+    end
+  end
+
+  scenario "with non-picture unsuccesfully" do
+    change_avatar("stylesheets", "application.scss")
+
+    expect(current_path).to eq "/users"
+    expect(page).to have_content "User could not be updated."
+    expect(page).to have_content "has an invalid file format"
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,28 @@
 require "rails_helper"
+require "refile/file_double"
 
 describe User do
   it { is_expected.to validate_presence_of :full_name }
   it { is_expected.to have_many :posts }
   it { is_expected.to have_many :comments }
+
+  context "avatar" do
+    let(:user) { create(:user) }
+
+    def upload_avatar(file, type)
+      user.update(avatar_image: Refile::FileDouble.new("dummy", file, type))
+    end
+
+    it "allows attaching an image" do
+      upload_avatar("logo.png", content_type: "image/png")
+
+      expect(user.avatar_image_id).not_to be_nil
+    end
+
+    it "doesn't allow attaching other files" do
+      upload_avatar("file.txt", content_type: "text/plain")
+
+      expect(user.avatar_image_id).to be_nil
+    end
+  end
 end
