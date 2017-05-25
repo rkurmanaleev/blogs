24ad01e9ffa7fe93897688fd1ca1453c1c652934
Ruslan Kurmanaleev <ruslan.kurmanaleev@yandex.ru>
2017-05-25 10:57:42 +0300

    Add Top Users (users#index)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,17 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   expose_decorated(:user)
+  expose_decorated(:users) { users_fetcher }
+
+  def index
+  end
+
+  def show
+  end
+
+  private
+
+  def users_fetcher
+    User.with_posts.sort_by(&:posts_count).reverse
+  end
 end
