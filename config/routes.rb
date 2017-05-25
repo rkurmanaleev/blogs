26cf0797207b44221ef621337b6aa52ad8c44c94
Ruslan Kurmanaleev <ruslan.kurmanaleev@yandex.ru>
2017-05-25 12:10:35 +0300

    Root_to My Posts Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,21 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "users/registrations" }
-  root to: "posts#index"
+  root to: "posts/my_posts#index"
 
   # posts/:id/comments
   resources :posts do
     resources :comments, only: %i(new create destroy)
   end
 
+  # posts/my_posts
+  resources :my_posts, module: "posts", controller: "my_posts", only: :index
+
   # users/:id/posts/all_posts
   # users/:id/posts/recent_posts
-  # users/:id/posts/my_posts
   resources :users, only: %i(index show) do
     namespace :posts, module: nil do
       resources :all_posts, controller: "users/posts/all_posts", only: :index
       resources :recent_posts, controller: "users/posts/recent_posts", only: :index
-      resources :my_posts, controller: "users/posts/my_posts", only: :index
     end
   end
 end
