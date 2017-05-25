f98f3fbef89e08a43fa2569d380d7054011643ac
Ruslan Kurmanaleev <ruslan.kurmanaleev@yandex.ru>
2017-05-25 10:59:29 +0300

    Refactor the Post Policy

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,10 +1,5 @@
 class PostPolicy < ApplicationPolicy
-  def initialize(user, post)
-    @user = user
-    @post = post
-  end
-
   def manage?
-    @post.user_id == @user.id
+    @record.user_id == @user.id
   end
 end
