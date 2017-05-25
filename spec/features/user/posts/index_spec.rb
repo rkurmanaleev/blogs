7ad363aa7ab6e5a02b04b040a1362283dcfeaa5f
Ruslan Kurmanaleev <ruslan.kurmanaleev@yandex.ru>
2017-05-25 11:17:01 +0300

    Refactor, add new Specs for User_Posts controllers

--- spec/features/user/posts/index_spec.rb
+++ spec/features/user/posts/index_spec.rb
@@ -1,14 +1,25 @@
 require "rails_helper"
 
 feature "List recent posts" do
-  context "Authorised User" do
-    include_context "current user signed in"
+  include_context "current user signed in"
+
+  context "User has posts" do
     let!(:post) { create(:post, user: current_user) }
 
     scenario "sees 10 last posts" do
       visit posts_path
+
       expect(page).to have_content(post.title)
       expect(page).to have_content(post.content)
+    end
+  end
+
+  context "User has no posts" do
+    scenario "sees a call2action" do
+      visit posts_path
+
+      expect(page).to have_content("Sorry, you have no posts yet...")
+      expect(page).to have_link("new post", href: new_post_path)
     end
   end
 end
