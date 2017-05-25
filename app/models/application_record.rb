c1141eb02683c0690ed0c79e94c7054a8e7d8474
Ruslan Kurmanaleev <ruslan.kurmanaleev@yandex.ru>
2017-05-25 15:01:58 +0300

    Refactor the scopes

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -3,4 +3,5 @@ class ApplicationRecord < ActiveRecord::Base
 
   scope :ordered_by_desc, -> { order(created_at: :desc) }
   scope :latest, -> { limit(10) }
+  scope :sorted_last, -> { ordered_by_desc.latest }
 end
