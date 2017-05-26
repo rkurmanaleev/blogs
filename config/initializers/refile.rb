a50a6e7404ad17ae9440b9a009c555f9e55520f2
Ruslan Kurmanaleev <ruslan.kurmanaleev@yandex.ru>
2017-05-26 11:51:32 +0300

    Set up Refile with Google Storage via Fog

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -1,1 +1,14 @@
+require "refile/fog"
 require "refile/simple_form"
+
+credentials = {
+  provider: "Google",
+  google_storage_access_key_id: ENV.fetch("GOOGLE_KEY_ID"),
+  google_storage_secret_access_key: ENV.fetch("GOOGLE_ACCESS_KEY"),
+  directory: ENV.fetch("GOOGLE_DIRECTORY")
+}
+
+Refile.configure do |config|
+  config.cache = Refile::Fog::Backend.new(prefix: "cache", **credentials)
+  config.store = Refile::Fog::Backend.new(prefix: "store", **credentials)
+end
