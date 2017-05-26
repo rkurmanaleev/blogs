7e28c1b8018b01f646690413b9130e2f4985b95b
Ruslan Kurmanaleev <ruslan.kurmanaleev@yandex.ru>
2017-05-26 12:15:56 +0300

    Refactor the Avatar specs

--- spec/features/user/account/avatar/destroy_spec.rb
+++ spec/features/user/account/avatar/destroy_spec.rb
@@ -9,8 +9,8 @@ feature "As the user I would like to remove my Avatar" do
     click_on "Update"
 
     expect(page).to have_content "Your account has been updated successfully."
+    expect(current_path).to eq edit_user_registration_path
 
-    visit edit_user_registration_path(current_user)
     within ".user-avatar" do
       expect(find("img")["src"]).not_to have_content "avatar_image.jpg"
       expect(find("img")["alt"]).to have_content "No avatar"
