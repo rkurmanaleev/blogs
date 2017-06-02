41cd33da2881537e7492cbaa9fe8ecc2546c90dc
Ruslan Kurmanaleev <ruslan.kurmanaleev@yandex.ru>
2017-06-02 12:38:22 +0300

    Remove unused scopes

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,7 +2,5 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
   scope :ordered_by_desc, -> { order(created_at: :desc) }
-  scope :latest, -> { limit(10) }
-  scope :sorted_last, -> { ordered_by_desc.latest }
   scope :sorted_recent, -> { ordered_by_desc.limit(5) }
 end
