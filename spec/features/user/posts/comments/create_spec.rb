f0226f75953a349de081578ece061676ec793711
Ruslan Kurmanaleev <ruslan.kurmanaleev@yandex.ru>
2017-06-18 20:23:36 +0300

    Feature - enable JS on new comment create

--- spec/features/user/posts/comments/create_spec.rb
+++ spec/features/user/posts/comments/create_spec.rb
@@ -3,16 +3,17 @@ require "rails_helper"
 feature "Create a comment" do
   include_context "current user signed in"
   include_context "current user's post"
-  let(:clean_form?) { page.has_field?("comment_content", with: "") }
+  let(:form_cleaned?) { page.has_field?("comment_content", with: "") }
 
   before { visit post_path(post) }
 
   scenario "User creates the comment with content", js: true do
-    fill_in("comment_content", with: "Some new and fresh comment")
+    fill_in "comment_content", with: "Some new and fresh comment"
+    expect(page).to have_button("Save the Comment", disabled: false)
 
     click_on "Save the Comment"
 
-    expect(clean_form?).to eq true
+    expect(form_cleaned?).to eq true
     expect(page).to have_content "Some new and fresh comment"
     expect(page).to have_content "Total comments 1"
   end
@@ -20,9 +21,7 @@ feature "Create a comment" do
   scenario "User creates the comment without content", js: true do
     fill_in "comment_content", with: nil
 
-    click_on "Save the Comment"
-
-    expect(page).to have_content "can't be blank"
+    expect(page).to have_button("Save the Comment", disabled: true)
     expect(page).to have_content "No comments yet..."
   end
 end
