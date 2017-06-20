2c7a4f6db94e379cb5c3a513799f1674001d6bf6
Ruslan Kurmanaleev <ruslan.kurmanaleev@yandex.ru>
2017-06-20 10:52:10 +0300

    Refactoring

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -5,7 +5,7 @@ aws = {
   access_key_id: ENV.fetch("AWS_ACCESS_KEY"),
   secret_access_key: ENV.fetch("AWS_SECRET_KEY"),
   region: ENV.fetch("AWS_REGION"),
-  bucket: ENV.fetch("AWS_BUCKET"),
+  bucket: ENV.fetch("AWS_BUCKET")
 }
 Refile.cache = Refile::S3.new(prefix: "cache", **aws)
 Refile.store = Refile::S3.new(prefix: "store", **aws)
